e8bd4c6bfda0766349e4653ff30e760002c1ea45
Alexander Shumenko <ashumenko@adyax.com>
2015-07-13 16:02:15 +0300

    Minor fix

--- themes/worthy/js/worthy.custom.js
+++ themes/worthy/js/worthy.custom.js
@@ -31,10 +31,17 @@ worthy_custom.attach = function ($context, settings) {
 
   // Attach home page banner.
   worthy_custom.attach_home_page_banner($context, settings);
+
+  // Attach main menu.
+  worthy_custom.attach_main_menu($context, settings);
 }
 
 worthy_custom.attach_home_page_banner = function ($context, settings) {
   $(".banner-image").backstretch(drupalSettings.path.path_to_theme + '/images/banner.jpg');
+}
+
+worthy_custom.attach_main_menu = function ($context, settings) {
+  $(".header .navbar ul").first().addClass('nav navbar-nav navbar-right');
 }
 
 })(jQuery, Drupal, drupalSettings);
