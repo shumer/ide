cbd13093cdcf09a990bb58eceec52ae34bc906a9
Alexander Shumenko <ashumenko@adyax.com>
2015-07-17 19:46:00 +0300

    Dashboard updates.

--- modules/custom/config_pages/src/ConfigPagesListBuilder.php
+++ modules/custom/config_pages/src/ConfigPagesListBuilder.php
@@ -9,6 +9,7 @@ namespace Drupal\config_pages;
 
 use Drupal\Core\Entity\EntityInterface;
 use Drupal\Core\Entity\EntityListBuilder;
+use Drupal\Core\Url;
 
 /**
  * Defines a class to build a listing of custom config_pages entities.
@@ -44,4 +45,47 @@ class ConfigPagesListBuilder extends EntityListBuilder {
     return $operations;
   }
 
+  /**
+   * {@inheritdoc}
+   */
+  public function getOperations(EntityInterface $entity) {
+    $path = $entity->menu['path'];
+    if (!empty($path)) {
+      $operations = [];
+      $operations['edit'] = [
+        'title' => t('Edit'),
+        'weight' => 10,
+        'query' => [],
+        'url' => Url::fromUserInput('/' . $path),
+      ];
+    }
+    uasort($operations, '\Drupal\Component\Utility\SortArray::sortByWeightElement');
+
+    return $operations;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function load() {
+    $entity_ids = $this->getEntityIds();
+    return \Drupal::entityManager()
+      ->getStorage('config_pages_type')->loadMultiple($entity_ids);
+  }
+
+  /**
+   * Loads entity IDs using a pager sorted by the entity id.
+   *
+   * @return array
+   *   An array of entity IDs.
+   */
+  protected function getEntityIds() {
+    $query = \Drupal::entityManager()
+      ->getStorage('config_pages_type')->getQuery();
+    $keys = $this->entityType->getKeys();
+    return $query
+      ->sort($keys['id'])
+      ->pager($this->limit)
+      ->execute();
+  }
 }
